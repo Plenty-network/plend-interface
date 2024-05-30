d896b828aba8e32f32b9b2ffda8b568d71f746bb
priyam-anand <priyam27.anand@gmail.com>
2024-05-30 17:09:22 +0530

    fix: typo in tooltip

--- src/components/infoTooltips/FrozenTooltip.tsx
+++ src/components/infoTooltips/FrozenTooltip.tsx
@@ -25,7 +25,7 @@ export const FrozenTooltip = ({ symbol, currentMarket }: FrozenTooltipProps) => 
       tooltipContent={
         <Box>
           <Trans>
-            This asset is frozen due to an Plend Protocol decision.{' '}
+            This asset is frozen due to Plend Protocol decision.{' '}
             <Link
               href={getFrozenProposalLink(symbol, currentMarket)}
               sx={{ textDecoration: 'underline' }}
